ced1832b053239e28edb15e84847faf8d404c931
jacobfield <jacobprogrammes@gmail.com>
2023-12-09 10:19:15 +0000

    added plan in comments

--- d20/src/components/AllDice.tsx
+++ d20/src/components/AllDice.tsx
@@ -7,3 +7,9 @@ export function AllDice(): JSX.Element {
     </div>
   );
 }
+
+// Plan
+// Add all other dice - d4, d6, d8, d10, d12, d100
+// Add option to roll with mods
+// Add option to roll multiple dice eg: 2d20
+// Add option to roll multiple types of dice eg: d20 + d10
--- d20/src/components/Rolld20.tsx
+++ d20/src/components/Rolld20.tsx
@@ -24,3 +24,9 @@ export function Rolld20(): JSX.Element {
     </div>
   );
 }
+
+// Plan
+// Add all other dice - d4, d6, d8, d10, d12, d100
+// Add option to roll with mods
+// Add option to roll multiple dice eg: 2d20
+// Add option to roll multiple types of dice eg: d20 + d10
