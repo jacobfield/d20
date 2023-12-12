fc2b831b3bd8baabcfc48030b948abde70cf0e7f
jacobfield <jacobprogrammes@gmail.com>
2023-12-12 17:24:56 +0000

    all dice functions made

---
+++ d20/src/components/RollD100.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export function Rolld100(): JSX.Element {
+  const [diceRoll, setDiceRoll] = useState<number>(randomNumber());
+
+  function randomNumber(): number {
+    let roll: number = Math.floor(Math.random() * 100) + 1;
+    console.log(roll);
+    return roll;
+  }
+  return (
+    <div>
+      <p>d100 = {diceRoll} </p>
+      <button onClick={() => setDiceRoll(randomNumber())}>Roll d100</button>
+    </div>
+  );
+}
